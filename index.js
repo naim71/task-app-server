7a482b5cc86d88c88f654f93af4902ec1bd6a8a8
naim71 <naimkhan1314@gmail.com>
2022-12-29 20:28:48 +0600

    upload

--- index.js
+++ index.js
@@ -20,11 +20,25 @@ async function run() {
         const usersCollection = client.db('task').collection('users');
 
         //CRUD operations
-        // app.get('/products', async (req, res) => {
-        //     const query = {};
-        //     const products = await productsCollection.find(query).toArray();
-        //     res.send(products);
+        app.get('/tasks', async (req, res) => {
+            const query = {};
+            const task = await tasksCollection.find(query).toArray();
+            res.send(task);
+        })
+
+        app.get('/task', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const tasks = await tasksCollection.find(query).toArray();
+            res.send(tasks);
+        });
+        //  app.get('/tasks/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const query = { email }
+        //     const result = await tasksCollection.findOne(query);
+        //     res.send(result);
         // })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
@@ -36,92 +50,33 @@ async function run() {
             const result = await tasksCollection.insertOne(task);
             res.send(result);
         })
-        // app.get('/products/:seller', async (req, res) => {
-        //     const seller = req.params.seller;
-        //     console.log(seller);
-        //     const query = { seller: seller }
-        //     const product = await productsCollection.find(query).toArray();
-        //     res.send(product);
-        // })
-        // app.put('/products/:seller', async (req, res) => {
-        //     const seller = req.params.seller;
-        //     const filter = { seller: seller }
-        //     const options = { upsert: true };
-        //     const updatedDoc = {
-        //         $set: {
-        //             verification: 'verified'
-        //         }
-        //     }
-        //     const result = await productsCollection.update(filter, updatedDoc, options);
-        //     res.send(result);
-        // })
-        // app.delete('/product/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: ObjectId(id) };
-        //     const result = await productsCollection.deleteOne(filter);
-        //     res.send(result);
-        // })
 
-
-        // app.get('/products/categories/:categoryId', async (req, res) => {
-        //     const id = req.params.categoryId;
-        //     console.log(id);
-        //     const query = { categoryId: id };
-        //     console.log(query);
-        //     const productresult = await productsCollection.find(query).toArray();
-        //     console.log(productresult);
-        //     res.send(productresult);
-        // })
-
-        // //checking if user is admin
-        // app.get('/users/admin/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const query = { email }
-        //     const user = await usersCollection.findOne(query);
-        //     res.send({ isAdmin: user?.role === 'admin' });
-        // })
-
-        // // app.get('/users/:id', async (req, res) => {
-        // //     const id = req.params.id;
-        // //     console.log(id);
-        // //     const query = { _id: ObjectId(id) }
-        // //     const user = await usersCollection.findOne(query);
-        // //     res.send(user);
-        // // })
-
-        // //delete seller
-        // app.delete('/users/seller/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const filter = { email: email };
-        //     const result = await usersCollection.deleteOne(filter);
-        //     res.send(result);
-        // })
-        // app.put('/users/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: ObjectId(id) }
-        //     const options = { upsert: true };
-        //     const updatedDoc = {
-        //         $set: {
-        //             status: 'verified'
-        //         }
-        //     }
-        //     const result = await usersCollection.updateOne(filter, updatedDoc, options);
-        //     res.send(result);
-        // })
-
-        // //jwt token
-        // app.get('/jwt', async (req, res) => {
-        //     const email = req.query.email;
-        //     const query = { email: email }
-        //     const user = await usersCollection.findOne(query);
-        //     if (user) {
-        //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '10h' })
-        //         return res.send({ accessToken: token })
-        //     }
-        //     console.log(user);
-        //     res.status(403).send({ accessToken: '' })
-        // })
-
+        app.get('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await tasksCollection.findOne(query);
+            res.send(result);
+        })
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await tasksCollection.deleteOne(filter);
+            res.send(result);
+        })
+      
+        app.put('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    status: 'completed'
+                }
+            }
+            const result = await tasksCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+       
     }
     finally {
 
@@ -129,8 +84,8 @@ async function run() {
 }
 run().catch(console.log);
 
-app.get('/', async(req, res) =>{
+app.get('/', async (req, res) => {
     res.send('Server is running');
 })
 
-app.listen(port, ()=> console.log(`Server running on ${port}`))
+app.listen(port, () => console.log(`Server running on ${port}`))
